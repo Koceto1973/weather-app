2b8166ec89720ca88410e49c915379e465d956d2
TeamK <koceto1973@yahoo.ca>
2018-12-31 10:57:30 +0200

    yargs for input and uri encoding

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+  var encodedAddress = encodeURIComponent(argv.address);
 
 request({
   // https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia  // over query limit
-  url: `http://www.mapquestapi.com/geocoding/v1/address?key=FWQcWRKAnhPCI2egavnMvrEEbpQ0RmAg&location=1301%20lombard%20street%20philadelphia`,
+  url: `http://www.mapquestapi.com/geocoding/v1/address?key=FWQcWRKAnhPCI2egavnMvrEEbpQ0RmAg&location=${encodedAddress}`,
   json: true  // convert to json directly, but clipped, not pretty
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].providedLocation.location}`);
